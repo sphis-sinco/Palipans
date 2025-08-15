63fba6a8c98580d6cec67439b4993225229450d2
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-15 07:55:51 -0500

    comics list script :D

---
+++ comicsList.js
@@ -0,0 +1,47 @@
+var skits = [
+        {
+                name: '',
+                release: "8/15/2025",
+                link: "index.html",
+        },
+];
+
+var addSkit = function (name = '', rel = "", href = "") {
+        skits.push({
+                name: name,
+                release: rel,
+                link: href,
+        });
+};
+
+skits = [];
+addSkit("Index", "8/15/2025", "index.html");
+
+var list = document.getElementById("comics-list");
+
+for (let skit of skits) {
+        var skitString =skit.name;
+
+        var card = document.createElement("li");
+        card.className = "card";
+
+        var skitElement = document.createElement("h4");
+        skitElement.innerHTML = skitString;
+
+        if (skit.release != null) {
+                skitElement.innerHTML += " (" + skit.release + ")";
+        }
+        card.appendChild(skitElement);
+
+        if (skit.release == null) {
+                var linkButton = document.createElement("button");
+                linkButton.innerHTML = "Coming Eventually";
+        } else {
+                var linkButton = document.createElement("a");
+                linkButton.innerHTML = "Go to " + skitString;
+                linkButton.href = skit.link;
+        }
+        card.appendChild(linkButton);
+
+        list.appendChild(card);
+}
